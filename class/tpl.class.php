f61cde8d851e983ccb8c99ed33bded089f36d9bc
杜世伟 <lovevipdsw@vip.qq.com>
2013-12-29 17:37:06 +0800

    upload 20131229 b1

--- class/tpl.class.php
+++ class/tpl.class.php
@@ -22,34 +22,27 @@ class tpl{
 	public static function dataTh($data){
 		return preg_replace(
 			array(
+				"/\{(.*?)\/\}/is", 
 				"/\<\!\-\-\#(.*?)\#\-\-\>/is",
 				"/\<\-\#(.*?)\#\-\>/is",
 				"/\<\%(.*?)\%\>/is",
-				"/\@site\:(.*?)\-\>/is",
-				"/\@theme\:(.*?)\-\>/is",
-				"/\{@url\:(.*?)\/(.*?)\/(.*?)\}/is",
-				"/\{@(.*?)\/\}/is",
 			),
 			array(
-				"<?php \\1?>",
-				"<?php \\1?>",
-				"<?php \\1?>",
-				G::site('base') . "/res/\\1/",
-				G::site('base') . "/control/" . POR . "/res/\\1/",
-				G::site('base') . "/\\1.php?m=\\1&c=\\2&a=\\3",
 				"<?php echo \\1;?>", 
+				"<?php \\1?>",
+				"<?php \\1?>",
+				"<?php \\1?>",
 			),
 			self::dataTplqr($data)
 		);
 	}
 	public static function dataTplqr($data){
 		return preg_replace(
-			"/\<tpl src\=\"(CCT|TPL)\:(.*?)\"\/\>/is",
-			"<?php include(tpl::tplby(\\1.G::url('\\2')));?>",
+			"/\<tpl src\=\"(CCT|TPL)\.(.*?)\"\/\>/is",
+			"<?php include(tpl::tplby(\\1.'\\2'));?>",
 			$data
 		);
 	}
-	
 	public static function tplby($url){
 		global $getBase;
 		self::dataTpl(DATATPL .  POR . '.M.' . $getBase['m'] . '.C.' . $getBase['c'] . '.A.' . $getBase['a'] . '.V.' . md5($url), fileDetact::dqwj($url, 'htm'));
